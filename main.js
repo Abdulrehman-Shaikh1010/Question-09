01978e3c45956bd102f6d71c5e4aec400a217945
Abdulrehman <you@example.com>
2024-04-06 14:53:00 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+var favNum = 5;
+var message = " My favourite Number is \"".concat(favNum, "\"");
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,17 @@
+const favNum: number = 5
+
+const message: string =` My favourite Number is "${favNum}"`;
+
+console.log(message);
+
+//echo "# Question-09" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Abdulrehman-Shaikh1010/Question-09.git
+// git push -u origin main
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/Abdulrehman-Shaikh1010/Question-09.git
+// git branch -M main
+// git push -u origin main
